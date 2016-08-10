b5863a8c2fb7ac583d5194cb877b0f03f3915f68
Stanislav Malyshev <stas@php.net>
2016-08-10 00:00:14 -0700

    Fix bug #72697: select_colors write out-of-bounds
    Assigned CVE-IDs: CVE-2016-7126
    
    cherry picked from commit a3598dd7c9b182debcb54b9322b1dece14c9b533)
    Signed-off-by: Lior Kaplan <kaplanlior@gmail.com>

--- ext/gd/gd.c
+++ ext/gd/gd.c
@@ -1693,11 +1693,11 @@ PHP_FUNCTION(imagetruecolortopalette)
 
 	ZEND_FETCH_RESOURCE(im, gdImagePtr, &IM, -1, "Image", le_gd);
 
-	if (ncolors <= 0) {
-		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Number of colors has to be greater than zero");
+	if (ncolors <= 0 || ncolors > INT_MAX) {
+		php_error_docref(NULL TSRMLS_CC, E_WARNING, "Number of colors has to be greater than zero and no more than %d", INT_MAX);
 		RETURN_FALSE;
 	}
-	gdImageTrueColorToPalette(im, dither, ncolors);
+	gdImageTrueColorToPalette(im, dither, (int)ncolors);
 
 	RETURN_TRUE;
 }
